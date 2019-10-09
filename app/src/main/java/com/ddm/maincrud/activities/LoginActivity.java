1d9fb46902eeb0fcfc81169cdd2e5460155005c9
Marco Antonio <marcoantoniozxcv@gmail.com>
2019-10-09 11:27:02 -0300

    Login

--- app/src/main/java/com/ddm/maincrud/activities/LoginActivity.java
+++ app/src/main/java/com/ddm/maincrud/activities/LoginActivity.java
@@ -1,15 +1,54 @@
 package com.ddm.maincrud.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ddm.maincrud.R;
+import com.ddm.maincrud.dto.DtoLogin;
+import com.ddm.maincrud.services.RetrofitService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String  TAG = "LoginActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void logar(View view) {
+        String email = ((EditText)findViewById(R.id.et_login_email)).getText().toString();
+        String senha = ((EditText)findViewById(R.id.et_login_senha)).getText().toString();
+
+        DtoLogin dtoLogin = new DtoLogin();
+        dtoLogin.setEmail(email);
+        dtoLogin.setPassword(senha);
+
+        RetrofitService.getServico(this).login(dtoLogin).enqueue(new Callback<DtoLogin>() {
+            @Override
+            public void onResponse(Call<DtoLogin> call, Response<DtoLogin> response) {
+                Toast.makeText(LoginActivity.this, "Usuario logado", Toast.LENGTH_SHORT).show();
+                SharedPreferences sp = getSharedPreferences("dados", 0);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("token",response.body().getToken());
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+
+            @Override
+            public void onFailure(Call<DtoLogin> call, Throwable t) {
+                Log.d(TAG, "onFailure: "+ t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/ddm/maincrud/services/InterfaceDeServicos.java
+++ app/src/main/java/com/ddm/maincrud/services/InterfaceDeServicos.java
@@ -1,5 +1,6 @@
 package com.ddm.maincrud.services;
 
+import com.ddm.maincrud.dto.DtoLogin;
 import com.ddm.maincrud.dto.DtoUser;
 
 import java.util.List;
@@ -13,5 +14,8 @@ public interface InterfaceDeServicos {
 
     @POST("/users")
     Call<DtoUser> cadastraUsuario(@Body DtoUser dtoUser);
+
+    @POST("/auth/login")
+    Call<DtoLogin> login(@Body DtoLogin dtoLogin);
 }
 
